7bb595c0bd15dc5d92191c795457e5700f10125d
Ricardo Filho <ricardinhogiasson16@gmail.com>
2021-07-15 16:39:05 -0300

    Add Header

--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,10 +1,28 @@
-import styled from 'styled-components/native'
+import styled from "styled-components/native";
+import { RFValue, RFPercentage } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
-  flex:1;
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-  
-  
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFValue(113)}PX;
+  background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: flex-end;
+  padding: ${RFValue(32)}px ${RFValue(16)}px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
