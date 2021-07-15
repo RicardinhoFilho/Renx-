50b55e0d67e33eb94f91632adbc13515832397ff
Ricardo Filho <ricardinhogiasson16@gmail.com>
2021-07-15 17:39:09 -0300

    Criando Card Do carro

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,8 +5,30 @@ import { RFValue } from "react-native-responsive-fontsize";
 import Logo from "../../assets/logo.svg";
 
 import { Container, Header, HeaderContent, TotalCars } from "./styles";
+import { Car } from "../../components/Car";
 
 export function Home() {
+//https://cdn.picpng.com/audi/audi-red-a5-28597.png
+  const car ={
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail:'https://cdn.picpng.com/audi/audi-red-a5-28597.png',
+  }
+
+  const car2 ={
+    brand: 'PORSHEE',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail:'https://freepngimg.com/thumb/porsche/9-2-porsche-png-picture.png',
+  }
+
   return (
     <Container>
       <StatusBar
@@ -20,6 +42,8 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+      <Car data={car}/>
+      <Car data={car2}/>
     </Container>
   );
 }
