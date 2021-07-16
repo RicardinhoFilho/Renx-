638a7b47f26f16c6fc69186686896001beae55df
Ricardo Filho <ricardinhogiasson16@gmail.com>
2021-07-16 10:36:16 -0300

    Adicionando e estilizando acessórios do carro em detalhes

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,8 +2,31 @@ import React from "react";
 
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
+import {Acessory} from "../../components/Acessory";
 
-import { Container, Header, CarImages } from "./style";
+import SpeedSvg from "../../assets/speed.svg";
+import AcelerateSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Acessoryes,
+
+} from "./style";
 
 export function CarDetails() {
   return (
@@ -19,6 +42,32 @@ export function CarDetails() {
           ]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+          <Acessoryes>
+            <Acessory name="380Km/h" icon={SpeedSvg}/>
+            <Acessory name="3.2S" icon={AcelerateSvg}/>
+            <Acessory name="800 HP" icon={ForceSvg}/>
+            <Acessory name="Gasolina" icon={GasolineSvg}/>
+            <Acessory name="Auto" icon={ExchangeSvg}/>
+            <Acessory name="2 pessoas" icon={PeopleSvg}/>
+          </Acessoryes>
+        <About>
+          Este é o automovel que surgiu do lendario touro lide indultado na
+          praça Real Maestranza
+        </About>
+      </Content>
     </Container>
   );
 }
