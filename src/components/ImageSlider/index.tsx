e54ffdc11d2e8e235c50131d461a54ba6aa8c3b6
Ricardo Filho <ricardinhogiasson16@gmail.com>
2021-07-29 05:49:40 -0300

    Listando carros da api

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -10,48 +10,46 @@ import {
 } from "./styles";
 
 interface Props {
-  imagesUrl: string[];
+  imagesUrl: {
+    id: string;
+    photo: string;
+  }[];
 }
 
-interface ChangeImageProps{
+interface ChangeImageProps {
   viewableItems: ViewToken[];
-  changed: ViewToken[]
+  changed: ViewToken[];
 }
 
 export function ImageSlider({ imagesUrl }: Props) {
+  const [imageIndex, setImageIndex] = useState(0);
 
-  const[imageIndex, setImageIndex] = useState(0);
-
-  const indexChanged = useRef((info: ChangeImageProps)=>{
+  const indexChanged = useRef((info: ChangeImageProps) => {
     //console.log(info);
-    const index = info.viewableItems[0].index!
+    const index = info.viewableItems[0].index!;
     setImageIndex(index);
   });
 
   return (
     <Container>
       <ImageIndexes>
-        {imagesUrl.map((_, index)=>(
-          <ImageIndex key={String(index)} active={index == imageIndex} />
+        {imagesUrl.map((item, index) => (
+          <ImageIndex key={item.id} active={index == imageIndex} />
         ))}
-        
-        
       </ImageIndexes>
 
-     
-        <FlatList
-          data={imagesUrl}
-          keyExtractor={(key) => key}
-          renderItem={({ item }) => (
-            <CarImageWrapper>
-            <CarImage source={{ uri: item }} resizeMode="contain" />
-            </CarImageWrapper>
-          )}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-           onViewableItemsChanged={indexChanged.current}
-        />
-      
+      <FlatList
+        data={imagesUrl}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <CarImageWrapper>
+            <CarImage source={{ uri: item.photo }} resizeMode="contain" />
+          </CarImageWrapper>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={indexChanged.current}
+      />
     </Container>
   );
 }
